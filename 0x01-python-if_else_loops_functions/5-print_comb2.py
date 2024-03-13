039c0f17e789a2afe20c40d7952dec4b3d5fd208
joshuayamoahyankson <ziriks360@gmail.com>
2024-03-13 05:47:48 +0000

    A program that prints numbers from 0 to 99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for digit in range(0, 100):
     if digit in [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]:
-        print(f"0{digit}")
+        print(f"0{digit},", end=" ")
     else:
-        print(f"{digit}")
+        print(f"{digit}", end=", " if digit != 99 else "\n")
