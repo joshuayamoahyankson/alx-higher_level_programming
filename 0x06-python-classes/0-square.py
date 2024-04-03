f64188703660bad52a97b97a626f4c2ba6925c11
joshuayamoahyankson <ziriks360@gmail.com>
2024-04-03 01:53:53 +0000

    Both task 0 and task 1 together with Readme file

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """A class defined with the name Square"""
 
+
 class Square:
     """does nothing for now"""
     pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """A class that defines a Square"""
 
+
 class Square:
     """A class by name Square"""
 
