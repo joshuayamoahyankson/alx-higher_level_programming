341cb99985ae22ef519f780765ced1f30e8762dc
joshuayamoahyankson <ziriks360@gmail.com>
2024-04-03 01:40:01 +0000

    Both task 0 and task 1 together with Readme file

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+class Square:
+    """A class by name Square"""
+    def __init__(self, size):
+        """A constructor to the class Square
+        intializing a private instance called size
+        as an argument"""
+        self.__size = size
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,3 @@
+Welcome to my ALX Projects/Tasks on Object Oriented Programming.
+This is an exciting journey.
+Join me as we unravel the mystery behind OOP!
