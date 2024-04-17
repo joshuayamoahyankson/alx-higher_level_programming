22a19ae90eb44a2eafccb30c49b0590518361dd3
joshuayamoahyankson <ziriks360@gmail.com>
2024-04-17 03:12:08 +0000

    Write a function that reads a text file (UTF8) and prints it to stdout

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+"""This module defines a text file for reading"""
+
+
 def read_file(filename=""):
+    """Using the with keyword, it opens in read-mode
+    and prints the contents of the text file"""
     with open(filename, encoding="utf-8") as text_file:
         read_text = text_file.read()
         print(read_text)
-        if read_text[-1] != '\n':
-            print()
     return read_text
