585a9248dd58991e61c2d74b8af7048c6884019f
joshuayamoahyankson <ziriks360@gmail.com>
2024-04-25 11:57:33 +0000

    A function that returns True/False depending on a condition

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module checks if an
+object is exactly an instance of the specified class"""
+
+
+def is_same_class(obj, a_class):
+    """Returns true or false depending on
+    whether the object is an instance of a
+    specified class or not"""
+    return type(obj) == a_class
