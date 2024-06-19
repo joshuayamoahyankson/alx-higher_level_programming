3a6c8e4d99c98d33802593d42aeeb25998562cf0
joshuayamoahyankson <ziriks360@gmail.com>
2024-06-19 04:12:05 +0000

    JavaScript WarmUp tasks solved

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -5,7 +5,7 @@ const args = process.argv;
 if (args.length <= 2) {
   console.log('No argument');
 } else if (args.length === 3) {
-  console.log('Arguments found');
+  console.log('Argument found');
 } else {
   console.log('Arguments found');
 }
