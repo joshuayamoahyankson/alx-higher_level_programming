762e642d7e8c04c010d7c802aecf94fa65a4a765
joshuayamoahyankson <ziriks360@gmail.com>
2024-06-28 04:21:12 +0000

    A similar script as task2 but is safe from MySQL injections

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -23,7 +23,7 @@ if __name__ == "__main__":
                          db=database_name, port=3306)
     cursor = db.cursor()
 
-    query = "SELECT * FROM states WHERE name LIKE BINARY '{}' \
+    query = "SELECT * FROM states WHERE name = %s \
             ORDER BY id ASC"
     cursor.execute(query, (state_name_searched))
     results = cursor.fetchall()
