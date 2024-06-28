c0b67fddaa966709f3604dd081a1e192d9994bc2
joshuayamoahyankson <ziriks360@gmail.com>
2024-06-28 07:09:14 +0000

    python file that contains the class definition of a State and...

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,17 @@
+#!usr/bin/python3
+"""A python file that contains the class definition
+of a State and an instance Base = declarative_base()
+"""
+
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+class State(Base):
+    """A definition af a class state
+    that inherits from Base
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, autoincrement=True, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
