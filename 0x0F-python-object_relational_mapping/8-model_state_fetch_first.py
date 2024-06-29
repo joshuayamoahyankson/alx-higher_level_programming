1b7e99198e4b0a880045522cdceab968911764e7
joshuayamoahyankson <ziriks360@gmail.com>
2024-06-29 05:49:20 +0000

    Few fix in indentation

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -24,8 +24,8 @@ if __name__ == '__main__':
     Session = sessionmaker(bind=engine)
     session = Session()
     state = session.query(State).order_by(State.id).first():
-        if state:
-            print("{}: {}".format(state.id, state.name))
-        else:
-            print("Nothing")
+    if state:
+        print("{}: {}".format(state.id, state.name))
+    else:
+        print("Nothing")
     session.close()
