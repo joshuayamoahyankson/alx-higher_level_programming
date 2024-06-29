257c33ee3aa9baf4ba36a522fc89a0fae87c1f34
joshuayamoahyankson <ziriks360@gmail.com>
2024-06-29 05:32:43 +0000

    A script that lists all State objects from the database hbtn_0e_6_usa

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""A script that lists all State
+objects from the database hbtn_0e_6_usa
+"""
+
+from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy import create_engine
+from sys import argv
+from model_state import Base, State
+
+if __name__ == '__main__':
+    """connects to the engine and retrieves needed information"""
+    mysql_username = argv[1]
+    mysql_password = argv[2]
+    database_name = argv[3]
+
+    db_connection = (
+        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+            mysql_username, mysql_password, database_name
+        )
+    )
+    engine = create_engine(db_connection, pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for state in session.query(State).order_by(State.id).all():
+        print("{}: {}".format(state.id, state.name))
+    session.close()
