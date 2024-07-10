c60be8d70a1c08706ca6771158e9b20110a7659c
joshuayamoahyankson <ziriks360@gmail.com>
2024-07-10 04:50:49 +0000

    made changes to tks 4, 6, and 7

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -10,6 +10,6 @@ import requests
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    email = {'email': sys.argv[2]}
-    response = requests.post(url, data=email)
+    email_var = {'email': sys.argv[2]}
+    response = requests.post(url, data=email_var)
     print(response.text)
--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -9,8 +9,9 @@ import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
+
     response = requests.get(url)
     if (response.status_code >= 400):
-        print(f"Error code: {response.status_code}")
+        print("Error code: {}".format(response.status_code))
     else:
-        print(response.content)
+        print(response.text)
