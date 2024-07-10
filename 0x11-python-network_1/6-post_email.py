5c72b3641fda27551a20419253e515712e68628e
joshuayamoahyankson <ziriks360@gmail.com>
2024-07-10 04:17:44 +0000

    few changes made to the email variable

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -9,6 +9,6 @@ import requests
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    email = sys.argv[2]
-    response = requests.post(url, data={'email': email})
+    email = ('email': sys.argv[2])
+    response = requests.post(url, data=email)
     print(response.content)
